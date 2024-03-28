45b82fceb00371970b5e975d33aa6f5e79be2b64
meshalalkhozai <meshalalkhozai@gmail.com>
2024-03-28 20:42:56 +0300

    Initial Commit

---
+++ icalories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  icalories
+//
+//  Created by meshal alkhozaei on 18/09/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ icalories/icaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  icaloriesApp.swift
+//  icalories
+//
+//  Created by meshal alkhozaei on 18/09/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct icaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
